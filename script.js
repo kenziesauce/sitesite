c170f796200f194a53b1518e69127b11972ca52d
kenziesauce <kenzie@miadolce.com>
2015-06-30 10:03:38 -0700

    changes

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-// create the module and name it scotchApp
 var scotchApp = angular.module('scotchApp', ['ngRoute']);
 
-// configure our routes
 scotchApp.config(function($routeProvider) {
     $routeProvider
 
-        // route for the home page
         .when('/', {
             templateUrl : 'pages/home.html',
             controller  : 'mainController'
         })
 
-        // route for the about page
         .when('/about', {
             templateUrl : 'pages/about.html',
             controller  : 'aboutController'
         })
 
-        // route for the contact page
         .when('/design', {
             templateUrl : 'pages/design.html',
             controller  : 'designController'
@@ -28,9 +23,7 @@ scotchApp.config(function($routeProvider) {
         });
 });
 
-// create the controller and inject Angular's $scope
 scotchApp.controller('mainController', function($scope) {
-    // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
 });
 
@@ -51,7 +44,6 @@ scotchApp.controller('designController', function($scope) {
 });
 
 scotchApp.controller('contactController', function($scope) {
-    //$scope.message = 'Projects!';
 
 
 });
