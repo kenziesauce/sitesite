714f8162abce0a165ea85847f23485afc24129ff
kenziesauce <kenzie@miadolce.com>
2015-07-01 18:11:37 -0700

    align

--- script.js
+++ script.js
@@ -60,3 +60,13 @@ $(document).ready(function ($) {
     });
 
 });
+
+
+$('.photos img').hover(
+    function(){
+        $(this).find('img').stop().fadeTo('slow', 0.4);
+    },
+    function(){
+        $(this).find('img').stop().fadeTo('slow', 1);
+    });
+
